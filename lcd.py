a68f979caa90d82491820fe5c7ab75c721818f6e
Jiansong Li <lijianso@usc.edu>
2023-05-04 11:47:19 -0700

    Final Change

--- lcd.py
+++ lcd.py
@@ -1,9 +1,9 @@
 import time
 from grove_rgb_lcd import *
 
-setRGB(0, 255, 0)
-file = open("./speech.txt", "r")
+setRGB(0, 255, 0)       #set RGB color for LCD
+file = open("./speech.txt", "r")        # read speech.txt from the current folder
 for text in file:
         time.sleep(3)
-        setText(text)
+        setText(text)   # print each line in the speech.txt
 file.close()
